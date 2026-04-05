e8f363e3049c564c48e96a5782bf19f550c1b893
Rianul Amin Rian <rianulamin.r@Rianuls-MacBook-Air.local>
2026-04-05 23:14:11 +0600

    database role updates

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -5,6 +5,8 @@ import { StripeEvent } from '../entities/stripe-event.entity';
 import { User } from '../entities/user.entity';
 import { Purchase } from '../entities/purchase.entity';
 import { Course } from '../entities/course.entity';
+import { Role } from '../entities/role.entity';
+import { Permission } from '../entities/permission.entity';
 
 export default new DataSource({
   type: 'postgres',
@@ -13,7 +15,7 @@ export default new DataSource({
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'mydb',
-  entities: [User, Account, StripeEvent, Purchase, Course],
+  entities: [User, Account, StripeEvent, Purchase, Course, Role, Permission],
   migrations: ['src/database/migrations/*.ts'],
   subscribers: [],
   logging: true,
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { Account } from 'src/entities/account.entity';
 import { User } from 'src/entities/user.entity';
+import { Role } from 'src/entities/role.entity';
+import { Permission } from 'src/entities/permission.entity';
 import { AuthController } from './auth.controller';
 import { appConfig } from 'src/config/app-config';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Account]),
+    TypeOrmModule.forFeature([User, Account, Role, Permission]),
     JwtModule.register({
       secret: appConfig.env.AUTH_SECRET,
       signOptions: { expiresIn: '1h' },
